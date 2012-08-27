5d34e72cd4f4921239272295db8598477b3c5f49
Matias H. Leidemer <matiasleidemer@gmail.com>
2012-08-26 23:34:20 -0300

    finished chapter 03

--- PRPFirstProjectTweeter/PRPViewController.m
+++ PRPFirstProjectTweeter/PRPViewController.m
@@ -11,12 +11,16 @@
 
 @interface PRPViewController ()
 
+- (void) reloadTweets;
+- (void) handleTwitterData: (NSData *)data
+               urlResponse: (NSHTTPURLResponse *)urlResponse
+                     error: (NSError *)error;
 @end
 
 @implementation PRPViewController
 
 //property name = instance variable name
-@synthesize twitterWebView = _twitterWebView;
+@synthesize twitterTextView = _twitterTextView;
 
 - (void)viewDidLoad
 {
@@ -39,6 +43,14 @@
     if ([TWTweetComposeViewController canSendTweet]) {
         TWTweetComposeViewController *tweetVC = [[TWTweetComposeViewController alloc] init];
         [tweetVC setInitialText: NSLocalizedString(@"I just finished the first project in iOS SDK Development. #meganti", nil)];
+        
+        tweetVC.completionHandler = ^(TWTweetComposeViewControllerResult result) {
+            if (result ==TWTweetComposeViewControllerResultDone) {
+                [self dismissModalViewControllerAnimated:YES];
+                [self reloadTweets];
+            }
+        };
+        
         [self presentViewController:tweetVC animated:YES completion:NULL];
     } else {
         NSLog(@"Can't send tweet");
@@ -46,12 +58,40 @@
 }
 
 - (IBAction)handleShowMyTweetsTapped:(id)sender {
-    NSURL *url = [NSURL URLWithString:@"http://www.twitter.com/matiasleidemer"];
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    [self.twitterWebView loadRequest:request];
+    [self reloadTweets];
 }
 
+- (void)reloadTweets {
+    NSURL *twitterAPIURL = [NSURL URLWithString:@"https://api.twitter.com/1/statuses/user_timeline.json"];
+    NSDictionary *twitterParams = [NSDictionary dictionaryWithObjectsAndKeys:@"matiasleidemer", @"screen_name", nil];
+    TWRequest *request = [[TWRequest alloc] initWithURL:twitterAPIURL parameters:twitterParams requestMethod:TWRequestMethodGET];
+    
+    [request performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error) {
+        [self handleTwitterData:responseData urlResponse:urlResponse error:error];
+    }];
+}
 
-
+- (void) handleTwitterData:(NSData *)data urlResponse:(NSHTTPURLResponse *)urlResponse error:(NSError *)error {
+    NSError *jsonError = nil;
+    NSJSONSerialization *jsonResponse = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+    
+    if (!jsonError && [jsonResponse isKindOfClass:[NSArray class]]) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSArray *tweets = (NSArray *) jsonResponse;
+            NSSortDescriptor *sortByText = [NSSortDescriptor sortDescriptorWithKey:@"text" ascending:YES];
+            NSArray *sortDescriptors = [NSArray arrayWithObjects: sortByText, nil];
+            tweets = [tweets sortedArrayUsingDescriptors:sortDescriptors];
+            
+            for (NSDictionary *tweetDict in tweets) {
+                NSString *tweetText = [NSString stringWithFormat:@"%@: %@ (%@)",
+                                       [tweetDict valueForKeyPath:@"user.name"],
+                                       [tweetDict valueForKey:@"text"],
+                                       [tweetDict valueForKey:@"created_at"]];
+                
+                self.twitterTextView.text = [NSString stringWithFormat:@"%@%@\n\n", self.twitterTextView.text, tweetText];
+            }
+        });
+    }
+}
 
 @end
